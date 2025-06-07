f3e9f2ea453fbe9a290b14bcb6dfe88e98d490f6
unknown <kathrynmwamburi@gmail.com>
2025-06-07 22:14:52 +0300

    commit

--- My Portfolio/src/Components/Navbar/Navbar.jsx
+++ My Portfolio/src/Components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import './Navbar.css';
 
 const Navbar = () => {
   const [darkMode, setDarkMode] = useState(true);
+  const [menuOpen, setMenuOpen] = useState(false); // NEW: Menu state
 
   useEffect(() => {
     document.body.className = darkMode ? 'dark' : 'light';
@@ -12,13 +13,21 @@ const Navbar = () => {
     <header className="header">
       <a href="#" className="logo-name">Kathryn Tokoli</a>
 
-     <ul className="nav-menu">
-  <li><a href="#home">Home</a></li>
-  <li><a href="#about">About Me</a></li>
-  <li><a href="#services">Services</a></li>
-  <li><a href="#portfolio">Portfolio</a></li>
-  <li><a href="#contact">Contact</a></li>
-</ul>
+      {/* Hamburger icon (visible on small screens) */}
+      <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
+      {/* Navigation menu */}
+      <ul className={`nav-menu ${menuOpen ? 'open' : ''}`}>
+        <li><a href="#home" onClick={() => setMenuOpen(false)}>Home</a></li>
+        <li><a href="#about" onClick={() => setMenuOpen(false)}>About Me</a></li>
+        <li><a href="#services" onClick={() => setMenuOpen(false)}>Services</a></li>
+        <li><a href="#portfolio" onClick={() => setMenuOpen(false)}>Portfolio</a></li>
+        <li><a href="#contact" onClick={() => setMenuOpen(false)}>Contact</a></li>
+      </ul>
 
       <div className="nav-right">
         <div className="nav-connect">Connect with me</div>
